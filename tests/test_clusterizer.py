72e09f32f8886ac6326afa16d73b17714d271ec7
DavidLP <pohl@physik.uni-bonn.de>
2015-12-02 14:59:00 +0100

    ENH: add a lot of test cases

--- tests/test_clusterizer.py
+++ tests/test_clusterizer.py
@@ -76,8 +76,118 @@ class TestClusterizer(unittest.TestCase):
         expected_result['n_cluster'] = 1
         # Test results
         self.assertEqual(cluster_hits.shape[0], 10)  # hit clustering not activated, thus this array has to be empty
-        self.assertTrue((cluster_hits == expected_result).all())
+        self.assertTrue(np.array_equal(cluster_hits, expected_result))
 
+    def test_cluster_cuts(self):
+        # create some fake data
+        hits = np.ones(shape=(2, ), dtype=data_struct.HitInfo)
+        hits[0]['column'], hits[0]['row'], hits[0]['charge'], hits[0]['event_number'] = 17, 36, 30, 19
+        hits[1]['column'], hits[1]['row'], hits[1]['charge'], hits[1]['event_number'] = 18, 36, 6, 19
+
+        # create clusterizer object
+        clusterizer = HitClusterizer(n_columns=100, n_rows=100)
+        clusterizer.create_cluster_hit_info_array(True)
+
+        # Case 1: Test max hit charge cut, accept all hits
+        clusterizer.set_max_hit_charge(30)  # only add hits with charge <= 30
+        clusterizer.add_hits(hits)  # cluster hits
+        # Check cluster
+        cluster = clusterizer.get_cluster()
+        expected_result = np.zeros(shape=(1, ), dtype=data_struct.ClusterInfo)
+        expected_result['event_number'] = [19]
+        expected_result['size'] = [2]
+        expected_result['charge'] = [36]
+        expected_result['seed_column'] = [17]
+        expected_result['seed_row'] = [36]
+        expected_result['mean_column'] = [17.68420982]
+        expected_result['mean_row'] = [36.5]
+        self.assertTrue(np.array_equal(cluster, expected_result))
+        # Check cluster hit info
+        cluster_hits = clusterizer.get_hit_cluster()
+        expected_result = np.zeros(shape=(2, ), dtype=data_struct.ClusterHitInfo)
+        expected_result['event_number'] = hits['event_number']
+        expected_result['frame'] = hits['frame']
+        expected_result['column'] = hits['column']
+        expected_result['row'] = hits['row']
+        expected_result['charge'] = hits['charge']
+        expected_result['is_seed'] = [1, 0]
+        expected_result['cluster_size'] = [2, 2]
+        expected_result['n_cluster'] = 1
+        self.assertTrue(np.array_equal(cluster_hits, expected_result))
+
+        # Case 2: Test max hit charge cut, omit charge > 29 hits
+        hits['event_number'] = 20
+        clusterizer.set_max_hit_charge(29)  # only add hits with charge <= 30
+        clusterizer.add_hits(hits)  # cluster hits
+        # Check cluster
+        cluster = clusterizer.get_cluster()
+        expected_result = np.zeros(shape=(1, ), dtype=data_struct.ClusterInfo)
+        expected_result['event_number'] = [20]
+        expected_result['size'] = [1]
+        expected_result['charge'] = [6]
+        expected_result['seed_column'] = [18]
+        expected_result['seed_row'] = [36]
+        expected_result['mean_column'] = [18.5]
+        expected_result['mean_row'] = [36.5]
+        self.assertTrue(np.array_equal(cluster, expected_result))
+        # Check cluster hit info
+        cluster_hits = clusterizer.get_hit_cluster()
+        expected_result = np.zeros(shape=(2, ), dtype=data_struct.ClusterHitInfo)
+        expected_result['event_number'] = hits['event_number']
+        expected_result['frame'] = hits['frame']
+        expected_result['column'] = hits['column']
+        expected_result['row'] = hits['row']
+        expected_result['charge'] = hits['charge']
+        expected_result['cluster_ID'] = [-1, 0]
+        expected_result['is_seed'] = [0, 1]
+        expected_result['cluster_size'] = [0, 1]
+        expected_result['n_cluster'] = [0, 1]
+        self.assertTrue(np.array_equal(cluster_hits, expected_result))
+
+    def test_not_implemented(self):
+        clusterizer = HitClusterizer(n_columns=1, n_rows=1)
+        with self.assertRaises(NotImplementedError):
+            clusterizer.set_max_cluster_charge(0)
+        with self.assertRaises(NotImplementedError):
+            clusterizer.set_max_cluster_hit_charge(0)
+
+    def test_exceptions(self):
+        with self.assertRaises(MemoryError):
+            HitClusterizer(n_columns=1000000, n_rows=1000000)  # Memory exception
+
+        # create some fake data
+        hits = np.ones(shape=(2, ), dtype=data_struct.HitInfo)
+        hits[0]['column'], hits[0]['row'], hits[0]['charge'], hits[0]['event_number'] = 17, 36, 3, 19
+        hits[1]['column'], hits[1]['row'], hits[1]['charge'], hits[1]['event_number'] = 18, 36, 6, 19
+
+        clusterizer = HitClusterizer(n_columns=10, n_rows=10)
+
+        with self.assertRaises(IndexError):
+            clusterizer.add_hits(hits)  # hits col/row too large
+
+        clusterizer = HitClusterizer(n_columns=100, n_rows=100)
+        clusterizer.set_cluster_info_array_size(0)  # set cluster array size to 0
+        hits['event_number'] = 20
+
+        with self.assertRaises(IndexError):
+            clusterizer.add_hits(hits)  # cluster array size too small
+
+        clusterizer.set_cluster_info_array_size(1)  # set cluster array size to 1
+        clusterizer.create_cluster_hit_info_array(True)
+        clusterizer.set_cluster_hit_info_array_size(0)  # set cluster array size to 0
+        hits['event_number'] = 21
+
+        with self.assertRaises(IndexError):
+            clusterizer.add_hits(hits)  # cluster hit array size too small
+
+        hits['event_number'] = 22
+        clusterizer.set_cluster_hit_info_array_size(2)  # set cluster array size to 0
+
+        with self.assertRaises(IndexError):
+            clusterizer.add_hits(hits)  # although the size fits now, there are old hit still stored, thus clusterizer.reset has to be called
+
+        clusterizer.reset()
+        clusterizer.add_hits(hits)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestClusterizer)
