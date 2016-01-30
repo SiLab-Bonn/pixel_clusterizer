963b275dd8d7487c9fa1a8414e3117769f11a109
DavidLP <david-leon.pohl@rub.de>
2016-01-30 09:57:53 +0100

    ENH: hit cluster info is set fully also for hits that are not assigned to a cluster (cluster_id -1) MAINT: deleting not needed funtions MAINT: comments

--- pixel_clusterizer/clusterizer.py
+++ pixel_clusterizer/clusterizer.py
@@ -24,55 +24,16 @@ def pprint_array(array):  # just to print the results in a nice way
 
 # Fast functions that are compiled in time via numba
 @njit()
-def _correct_cluster_id(hits, actual_event_number, actual_cluster_id, actual_event_hit_index, cluster_id):
-    ' Substracts one from all cluster IDs of the event, starting from cluster id actual_cluster_id'
-    for i in range(actual_event_hit_index, hits.shape[0]):
-        if _new_event(hits[i].event_number, actual_event_number):  # Stop if new event is reached
-            break
-        if cluster_id[i] != -1 and cluster_id[i] > actual_cluster_id:
-            cluster_id[i] -= 1
-
-
-@njit()
 def _new_event(event_number, actual_event_number):
     'Detect a new event by checking if the event number of the actual hit is the actual event number'
     return event_number != actual_event_number
 
 
 @njit()
-def _merge_cluster(i, j, hits, cluster, is_seed, cluster_id, max_cluster_charge, next_cluster_id, actual_event_cluster_index):
-    # is_seed[j] = 0  # Event hit is not necessarily seed anymore
-    if hits[j].charge >= max_cluster_charge:  # Old cluster hit can be the seed, if charge is equal max_cluster_charge to keep lowest index max charge hit seed hit
-        max_cluster_charge = hits[j].charge
-        is_seed[j] = 1
-    actual_cluster_id = cluster_id[j]  # Correct the actual cluster id
-
-    # Merge cluster infos
-    cluster[actual_event_cluster_index + cluster_id[j]].n_hits += cluster[actual_event_cluster_index + cluster_id[i]].n_hits  # Add up cluster sizes of the merged cluster
-    cluster[actual_event_cluster_index + actual_cluster_id].charge += cluster[actual_event_cluster_index + cluster_id[i]].charge  # Add up cluster charges of the merged cluster
-
-    # Reset wrongly set new cluster value after merge
-    cluster[actual_event_cluster_index + cluster_id[i]].n_hits = 0  # Reset wrongly set cluster value
-    cluster[actual_event_cluster_index + cluster_id[i]].ID = 0  # Reset wrongly set cluster value
-    cluster[actual_event_cluster_index + cluster_id[i]].charge = 0  # Reset wrongly set cluster value
-
-    # Correct cluster IDs of the hit / cluster array
-    cluster[actual_event_cluster_index + actual_cluster_id].ID = actual_cluster_id  # Set ID of actual cluster
-    cluster_id[i] = actual_cluster_id  # Actual hit belongs to other already existing cluster
-
-    next_cluster_id -= 1  # Actual new cluster ID is not used, since actual hit belongs to already existing cluster
-
-    return max_cluster_charge, next_cluster_id, actual_cluster_id
-
-
-@njit()
 def _finish_event(hits, cluster, is_seed, n_cluster, cluster_size, cluster_id, actual_event_hit_index, new_actual_event_hit_index, next_cluster_id, actual_event_cluster_index):
     ''' Set hit and cluster information of the last finished event (like number of cluster in this event (n_cluster),  cluster charge ...). '''
-    for i in range(actual_event_hit_index, new_actual_event_hit_index):
-        # Set hit cluster info that is only known at the end of the event
-        # Set it only for hits that are assigned to a cluster (TODO: do not do this anymore, done for compatibility)
-        if cluster_id[i] != -1:
-            n_cluster[i] = next_cluster_id
+    for i in range(actual_event_hit_index, new_actual_event_hit_index):  # Set hit cluster info that is only known at the end of the event
+        n_cluster[i] = next_cluster_id
 
     # Normalize cluster position by the charge for center of gravity
     for i in range(actual_event_cluster_index, actual_event_cluster_index + next_cluster_id):
@@ -82,14 +43,15 @@ def _finish_event(hits, cluster, is_seed, n_cluster, cluster_size, cluster_id, a
 
 @njit()
 def _reset_cluster_hit_indices(actual_cluster_hit_indices, actual_cluster_size):
+    ''' Sets the cluster hit indices to the std. valie -1. To be able to use this array in the new event'''
     for i in range(actual_cluster_size):
         actual_cluster_hit_indices[i] = -1
 
 
 @njit()
-def is_in_max_difference(value_1, value_2, max_difference):
+def _is_in_max_difference(value_1, value_2, max_difference):
     ''' Helper function to determine the difference of two values that can be np.uints. Works in python and numba mode.
-    Circumvents the numba bug #1653'''
+    Circumvents numba bug #1653'''
     if value_1 <= value_2:
         return value_2 - value_1 <= max_difference
     return value_1 - value_2 <= max_difference
@@ -97,6 +59,7 @@ def is_in_max_difference(value_1, value_2, max_difference):
 
 @njit()
 def cluster_hits(hits, cluster, n_hits, x_cluster_distance=1, y_cluster_distance=2, frame_cluster_distance=4, max_n_cluster_hits=30, max_cluster_hit_charge=13, ignore_same_hits=True):
+    ''' Main precompiled function that loopes over the hits and clusters them '''
     # Additional cluster info for the hit array
     cluster_id = np.zeros(shape=hits.shape, dtype=np.int16) - 1  # Cluster ID -1 means hit not assigned to cluster
     is_seed = np.zeros(shape=hits.shape, dtype=np.uint8)  # Seed 1 means hit is seed; lowest index hit with max charge hit is seed, thus there is always only one seed in a cluster
@@ -181,7 +144,7 @@ def cluster_hits(hits, cluster, n_hits, x_cluster_distance=1, y_cluster_distance
                     break
 
                 # Check if event hit belongs to actual hit and thus to the actual cluster
-                if is_in_max_difference(hits[actual_inner_loop_hit_index].column, hits[k].column, x_cluster_distance) and is_in_max_difference(hits[actual_inner_loop_hit_index].row, hits[k].row, y_cluster_distance) and is_in_max_difference(hits[actual_inner_loop_hit_index].frame, hits[k].frame, frame_cluster_distance):
+                if _is_in_max_difference(hits[actual_inner_loop_hit_index].column, hits[k].column, x_cluster_distance) and _is_in_max_difference(hits[actual_inner_loop_hit_index].row, hits[k].row, y_cluster_distance) and _is_in_max_difference(hits[actual_inner_loop_hit_index].frame, hits[k].frame, frame_cluster_distance):
                     if not ignore_same_hits or hits[actual_inner_loop_hit_index].column != hits[k].column or hits[actual_inner_loop_hit_index].row != hits[k].row:
                         actual_cluster_size += 1
                         actual_cluster_hit_index += 1
@@ -222,7 +185,7 @@ def cluster_hits(hits, cluster, n_hits, x_cluster_distance=1, y_cluster_distance
 
 
 class HitClusterizer(object):
-
+    ''' Clusterizer class providing an interface for the jitted functions and storing settings.'''
     def __init__(self, max_hits=10000):
         # Std. settings
         self._create_cluster_hit_info_array = False
@@ -294,21 +257,3 @@ class HitClusterizer(object):
         self.cluster = np.zeros(shape=(self._max_hits, ), dtype=data_struct.ClusterInfo).view(np.recarray)  # Only recarrays no structured arrays are supported by numba
         self.n_cluster = 0
 
-if __name__ == '__main__':
-
-    def create_hits(n_hits, max_column, max_row, max_frame, max_charge):
-        hits = np.ones(shape=(n_hits, ), dtype=data_struct.HitInfo)
-        for i in range(n_hits):
-            hits[i]['event_number'], hits[i]['frame'], hits[i]['column'], hits[i]['row'], hits[i]['charge'] = i / 3, i % max_frame, i % max_column + 1, 2 * i % max_row + 1, i % max_charge
-        return hits
-
-    hits = create_hits(n_hits=10, max_column=100, max_row=100, max_frame=1, max_charge=2)
-
-    clusterizer = HitClusterizer()
-    clusterizer.set_max_cluster_hits(1)
-
-    hits_clustered, cluster = clusterizer.cluster_hits(hits)
-
-    pprint_array(hits)
-    pprint_array(hits_clustered)
-    pprint_array(cluster)
