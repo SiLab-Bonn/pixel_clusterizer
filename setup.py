0f27fb1a1a83802dc000b81db7e8d37e8a0ba263
DavidLP <david-leon.pohl@rub.de>
2016-01-30 22:07:36 +0100

    MAINT: increase version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages  # This setup relies on setuptools since distutils is insufficient and badly hacked code
 
-version = '2.0.0'
+version = '2.1.0'
 author = 'David-Leon Pohl'
 author_email = 'pohl@physik.uni-bonn.de'
 
