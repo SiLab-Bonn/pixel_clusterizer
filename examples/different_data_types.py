3e7b70483d6f02783e8f465fb41a0f2c9f0a2ab2
DavidLP <david-leon.pohl@rub.de>
2016-02-01 13:06:17 +0100

    MAINT: show how to use custom cluster struct

--- examples/different_data_types.py
+++ examples/different_data_types.py
@@ -22,20 +22,44 @@ def pprint_array(array):  # Just to print the arrays in a nice way
 
 
 if __name__ == "__main__":
-    # A custom hit structure is defined here with unique names and data types
+    # A custom hit/cluster structure is defined here with unique names and data types
     hit_dtype = np.dtype([('timestamp', '<f8'),
                           ('time_window', '<u1'),
                           ('x', '<u4'),
                           ('y', '<u4'),
                           ('tot', '<u1')])
 
-    # A mapping to the internal hit field has to be done
+    cluster_dtype = np.dtype([('timestamp', '<f8'),
+                              ('x', '<u4'),
+                              ('y', '<u4'),
+                              ('tot', '<u1'),
+                              ('size', '<u2'),
+                              ('ID', '<u2'),
+                              ('seed_column', '<u2'),
+                              ('seed_row', '<u2'),
+                              ('mean_column', 'f4'),
+                              ('mean_row', 'f4'),
+                              ('unused_parameter', 'f4')])
+
+    # A mapping to the internal hit/cluster field names has to be done
     hit_fields = {'timestamp': 'event_number',
                   'x': 'column',
                   'y': 'row',
                   'tot': 'charge',
                   'time_window': 'frame'
                   }
+
+    cluster_fields = {'timestamp': 'event_number',
+                      'x': 'column',
+                      'y': 'row',
+                      'tot': 'charge',
+                      'size': 'n_hits',
+                      'ID': 'ID',
+                      'seed_column': 'seed_column',
+                      'seed_row': 'seed_row',
+                      'mean_column': 'mean_column',
+                      'mean_row': 'mean_row'
+                      }
 
     # Create some fake data
     hits = np.ones(shape=(3, ), dtype=hit_dtype)
@@ -44,7 +68,10 @@ if __name__ == "__main__":
     hits[2]['x'], hits[2]['y'], hits[2]['tot'], hits[2]['timestamp'] = 7, 7, 1, 1.1
 
     # Initialize clusterizer object
-    clusterizer = HitClusterizer(hit_fields=hit_fields, hit_dtype=hit_dtype)
+    clusterizer = HitClusterizer(hit_fields=hit_fields,
+                                 hit_dtype=hit_dtype,
+                                 cluster_fields=cluster_fields,
+                                 cluster_dtype=cluster_dtype)
 
     # Main function
     cluster_hits, cluster = clusterizer.cluster_hits(hits)  # cluster hits
